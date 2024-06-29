6d24b1da152197641b7bad9b21ea1fcb363d672f
sapzilking <sapzilking@gmail.com>
2024-06-29 21:46:47 +0900

    Redis List 관련 연습

---
+++ src/main/java/org/example/ListAndSet.java
@@ -0,0 +1,50 @@
+package org.example;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+import java.util.List;
+
+public class ListAndSet {
+
+    public static void ListAndSetTest() {
+        try (var jedisPool = new JedisPool("127.0.0.1", 6379)) {
+            try (Jedis jedis = jedisPool.getResource()) {
+                // list
+                // 1. stack
+                jedis.rpush("stack1", "a");
+                jedis.rpush("stack1", "b");
+                jedis.rpush("stack1", "c");
+
+                System.out.println(jedis.rpop("stack1"));
+                System.out.println(jedis.rpop("stack1"));
+                System.out.println(jedis.rpop("stack1"));
+
+                List<String> stack1 = jedis.lrange("stack1", 0, -1);
+                stack1.forEach(System.out::println);
+
+                // 2. queue
+                jedis.rpush("queue1", "d");
+                jedis.rpush("queue1", "e");
+                jedis.rpush("queue1", "f");
+
+                System.out.println(jedis.lpop("queue1"));
+                System.out.println(jedis.lpop("queue1"));
+                System.out.println(jedis.lpop("queue1"));
+
+//                List<String> queue1 = jedis.lrange("queue1", 0, -1);
+//                queue1.forEach(System.out::println);
+
+                // 3. block brpop
+                while (true) {
+                    List<String> blpop = jedis.blpop(10, "queue:blocking");
+                    if (blpop != null) {
+                        blpop.forEach(System.out::println);
+                    }
+                }
+            }
+        }
+
+
+    }
+}
