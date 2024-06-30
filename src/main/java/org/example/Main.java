b7dada98c07641cabcb7867e44dfe1bf70bc8091
sapzilking <sapzilking@gmail.com>
2024-06-30 16:22:35 +0900

    Redisi Hash 실습

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 //        RedisString.StringTest();
-        ListAndSet.ListAndSetTest();
+//        ListAndSet.ListAndSetTest();
+        RedisHash.hashTest();
+
     }
 }
